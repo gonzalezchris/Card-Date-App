b5c5fdd58435efdf84db5d8886565e7431cba81e
Chris Gonzalez <developerchriis@gmail.com>
2018-03-23 18:58:27 -0400

    Initial Commit

---
+++ Card Date App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Card Date App
+//
+//  Created by Christoph on 3/23/18.
+//  Copyright © 2018 Christoph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Card Date AppTests/Card_Date_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Card_Date_AppTests.swift
+//  Card Date AppTests
+//
+//  Created by Christoph on 3/23/18.
+//  Copyright © 2018 Christoph. All rights reserved.
+//
+
+import XCTest
+@testable import Card_Date_App
+
+class Card_Date_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
